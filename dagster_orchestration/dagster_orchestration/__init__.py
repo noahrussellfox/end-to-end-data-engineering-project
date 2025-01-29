4757bd7deba321c810233ff86c1de4398105f746
Noah <rfnoah@gmail.com>
2025-01-28 21:39:35 -0700

    added airbyte and dagster schedule

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,6 +1,13 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, AssetSelection, ScheduleDefinition, load_assets_from_modules, define_asset_job
 
 from .assets import resources
 
+faker_job = define_asset_job("faker_job", selection=AssetSelection.all())
+
+faker_schedule = ScheduleDefinition(
+    job=faker_job,
+    cron_schedule="0 0 * * *"  # every day
+)
+
 defs = Definitions(assets=load_assets_from_modules(
-    [assets]), resources=resources)
+    [assets]), resources=resources, jobs=[faker_job], schedules=[faker_schedule])
